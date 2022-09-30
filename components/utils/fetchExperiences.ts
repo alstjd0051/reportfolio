eeff9a6749850c9e4415c15a5f6d79f1f0fe250b
alstjd0051 <wsc03002@naver.com>
2022-09-30 18:36:07 +0900

    delete:: '/'

--- components/utils/fetchExperiences.ts
+++ components/utils/fetchExperiences.ts
@@ -2,7 +2,7 @@ import { Experience } from "../lib/typings";
 
 export const fetchExperiences = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperince`
+    `${process.env.NEXT_PUBLIC_BASE_URL}api/getExperince`
   );
 
   const data = await res.json();
--- components/utils/fetchPageInfo.ts
+++ components/utils/fetchPageInfo.ts
@@ -1,9 +1,7 @@
 import { PaegInfo } from "../lib/typings";
 
 export const fetchPageInfo = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getPageInfo`);
   const data = await res.json();
 
   const pageInfo: PaegInfo = data.pageInfo;
