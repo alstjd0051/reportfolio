30c7b79566b9aa4960a3b68ce5a0ab92c7dad852
alstjd0051 <wsc03002@naver.com>
2022-10-01 01:52:37 +0900

    Update:: fetch Props

--- components/utils/fetchSocials.ts
+++ components/utils/fetchSocials.ts
@@ -1,12 +1,6 @@
-import { Social } from "../lib/typings";
+import axios from 'axios'
 
-export const fetchSocials = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getSocials`);
-
-  const data = await res.json();
-  const socials: Social[] = data.socials;
-
-  //   console.log("fetching", socials);
-
-  return socials;
-};
+export default async function fetchSocials() {
+  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`)
+  return data.socials
+}
