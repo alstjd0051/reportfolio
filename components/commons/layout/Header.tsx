1dce2735f48ca44f37d936cb5a96f57a8a5a7575
alstjd0051 <wsc03002@naver.com>
2022-10-13 03:36:28 +0900

    update:: loginFuc

--- components/commons/layout/Header.tsx
+++ components/commons/layout/Header.tsx
@@ -3,12 +3,15 @@ import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { Social } from "../../lib/typings";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 type Props = {
   socials: Social[];
 };
 
 const Header = ({ socials }: Props) => {
+  const { data: session } = useSession();
+  console.log("HERE IS DATA", session);
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
@@ -27,25 +30,43 @@ const Header = ({ socials }: Props) => {
           />
         ))}
       </motion.div>
-      <Link href="#contact">
+      <div className="flex items-center justify-between">
+        <Link href="#contact">
+          <motion.div
+            initial={{ x: 500, opacity: 0, scale: 0.5 }}
+            animate={{ x: 0, opacity: 1, scale: 1 }}
+            transition={{ duration: 1.5 }}
+            className="flex flex-row items-center text-gray-300 cursor-pointer mx-5 "
+          >
+            {/* Nav */}
+            <SocialIcon
+              className="cursor-pointer"
+              network="email"
+              fgColor="gray"
+              bgColor="transparent"
+            />
+            <p className="uppercase hidden md:inline-flex text-sm text-gray-400 ">
+              Get in Touch
+            </p>
+          </motion.div>
+        </Link>
         <motion.div
           initial={{ x: 500, opacity: 0, scale: 0.5 }}
           animate={{ x: 0, opacity: 1, scale: 1 }}
           transition={{ duration: 1.5 }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer"
         >
-          {/* Nav */}
-          <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            fgColor="gray"
-            bgColor="transparent"
-          />
-          <p className="uppercase hidden md:inline-flex text-sm text-gray-400 ">
-            Get in Touch
-          </p>
+          <div className="flex flex-col  items-center justify-center  ">
+            {session?.user ? (
+              <>
+                <p>{session?.user.name}</p>
+                <button onClick={() => signOut()}>Sign Out</button>
+              </>
+            ) : (
+              <button onClick={() => signIn("google")}>Sign In</button>
+            )}
+          </div>
         </motion.div>
-      </Link>
+      </div>
     </header>
   );
 };
