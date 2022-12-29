6486bd4ffef65adb0e72b2b246ef30a5f7fcff89
alstjd0051 <wsc03002@naver.com>
2022-12-29 13:26:21 +0900

    delete reactQueryDevtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,6 @@ export default function MyApp({
       <div className="z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[#242424] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#6667ab] ">
         <QueryClientProvider client={queryClient}>
           <Component {...pageProps} />
-          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </div>
     </SessionProvider>
