7a76e528497db17b0a95aba5f9065ccf608e6857
alstjd0051 <wsc03002@naver.com>
2022-12-29 15:28:47 +0900

    update:: Controll process

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,43 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import type { AppContext, AppInitialProps, AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { defaultComponents } from '@portabletext/react';
+import { NextComponentType } from 'next';
 
-export default function MyApp({
+const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
-  pageProps: { ...pageProps },
-}: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
+  pageProps,
+}) => {
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
 
   return (
     <SessionProvider>
       <div className="z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[#242424] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#6667ab] ">
-        <QueryClientProvider client={queryClient}>
+        <QueryClientProvider client={queryClientRef.current}>
           <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </div>
     </SessionProvider>
   );
-}
+};
+
+MyApp.getInitialProps = async ({
+  Component,
+  ctx,
+}: AppContext): Promise<AppInitialProps> => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+};
+
+export default MyApp;
