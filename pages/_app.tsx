e706cfc88232c92a5478b87f17ee1d69c1bebf25
Tyler <wsc03002@example.com>
2023-01-18 18:40:10 +0900

    update:: adSense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { useRef, useState } from "react";
 import { defaultComponents } from "@portabletext/react";
 import { NextComponentType } from "next";
+import Script from "next/script";
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
@@ -18,16 +19,19 @@ const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
 
   return (
     <SessionProvider>
-      <div className="z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[#242424] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#6667ab] ">
+      <div className="z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[#242424] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#6667ab] relative">
+        <Script
+          style={{ display: "block", position: "absolute" }}
+          id="AdsenseId"
+          data-ad-client="ca-pub-8322923444929146"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          crossOrigin="anonymous"
+          async
+        />
         <QueryClientProvider client={queryClientRef.current}>
           <Component {...pageProps} />
         </QueryClientProvider>
       </div>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8322923444929146"
-        crossOrigin="anonymous"
-      ></script>
     </SessionProvider>
   );
 };
