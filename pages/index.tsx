7369d02b1efc9f47a91e3a21b0ed03d17a4e722a
Tyler <wsc03002@example.com>
2023-01-18 10:56:12 +0900

    update:: Adsense

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
-import { ArrowUpIcon } from '@heroicons/react/24/solid';
-import type { GetServerSideProps, GetStaticProps, NextPage } from 'next';
-import Head from 'next/head';
-import About from '../components/commons/about/AboutUI';
-import ContactMe from '../components/commons/contact/Contact';
-import WorkExperience from '../components/commons/experience/WorkExperience';
-import HeroUI from '../components/commons/Hero/HeroUI';
-import Header from '../components/commons/layout/Header';
-import Projects from '../components/commons/projects/Projects';
-import SkillUI from '../components/commons/skill/SkillUI';
+import { ArrowUpIcon } from "@heroicons/react/24/solid";
+import type { GetServerSideProps, GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import About from "../components/commons/about/AboutUI";
+import ContactMe from "../components/commons/contact/Contact";
+import WorkExperience from "../components/commons/experience/WorkExperience";
+import HeroUI from "../components/commons/Hero/HeroUI";
+import Header from "../components/commons/layout/Header";
+import Projects from "../components/commons/projects/Projects";
+import SkillUI from "../components/commons/skill/SkillUI";
 import {
   Experience,
   PageInfo,
   Project,
   Skill,
   Social,
-} from '../components/lib/typings';
-import { fetchExperiences } from '../components/utils/fetchExperiences';
-import fetchPageInfo from '../components/utils/fetchPageInfo';
-import fetchProjects from '../components/utils/fetchProjects';
-import fetchSkills from '../components/utils/fetchSkills';
-import fetchSocials from '../components/utils/fetchSocials';
+} from "../components/lib/typings";
+import { fetchExperiences } from "../components/utils/fetchExperiences";
+import fetchPageInfo from "../components/utils/fetchPageInfo";
+import fetchProjects from "../components/utils/fetchProjects";
+import fetchSkills from "../components/utils/fetchSkills";
+import fetchSocials from "../components/utils/fetchSocials";
 
 type Props = {
   pageInfo: PageInfo;
@@ -64,6 +64,11 @@ const Home = ({ pageInfo, experience, projects, skills, socials }: Props) => {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8322923444929146"
+        crossOrigin="anonymous"
+      ></script>
     </div>
   );
 };
