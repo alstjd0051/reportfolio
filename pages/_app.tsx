17a133c11610806e1b20b53cd83fd21e7b765ae8
Tyler <wsc03002@example.com>
2023-01-18 10:55:58 +0900

    delete:: reactqueryDevtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import '../styles/globals.css';
-import type { AppContext, AppInitialProps, AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { useRef, useState } from 'react';
-import { defaultComponents } from '@portabletext/react';
-import { NextComponentType } from 'next';
+import "../styles/globals.css";
+import type { AppContext, AppInitialProps, AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { useRef, useState } from "react";
+import { defaultComponents } from "@portabletext/react";
+import { NextComponentType } from "next";
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
@@ -21,7 +21,6 @@ const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
       <div className="z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[#242424] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#6667ab] ">
         <QueryClientProvider client={queryClientRef.current}>
           <Component {...pageProps} />
-          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </div>
     </SessionProvider>
