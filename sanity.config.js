bee14ccaeaa9ca8a4b7ebc1ace1b7a9e2712f423
alstjd0051 <wsc03002@naver.com>
2023-06-27 17:38:10 +0900

    UPDATE:: lib(codeInput)

--- sanity.config.js
+++ sanity.config.js
@@ -1,44 +1,24 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import schema from "./schemas/schema";
-import deskStructure from "./schemas/deskStructure";
 import { visionTool } from "@sanity/vision";
+import { codeInput } from "@sanity/code-input";
 
 export default defineConfig({
   title: "portfolio2.0",
   projectId: "kfie2jqu",
   dataset: "production",
-  plugins: [
-    deskTool({
-      structure: deskStructure,
-    }),
-    visionTool(),
-  ],
-  tools: (prev) => {
-    if (import.meta.env.DEV) {
+  plugins: [deskTool(), visionTool(), codeInput()],
+  tools: (prev, context) => {
+    const isAdmin = context.currentUser.roles.find(
+      ({ name }) => name === "administrator"
+    );
+    if (isAdmin) {
       return prev;
     }
     return prev.filter((tool) => tool.name !== "vision");
   },
   schema: {
     types: schema,
-  },
-  document: {
-    newDocumentOptions: (prev, { creationContext }) => {
-      if (creationContext.type === "global") {
-        return prev.filter(
-          (templateItem) => templateItem.templateId != "settings"
-        );
-      }
-      return prev;
-    },
-    actions: (prev, { schemaType }) => {
-      if (schemaType === "settings") {
-        return prev.fill(
-          ({ action }) => !["unpublish", "delete", "duplicate"].includes(action)
-        );
-      }
-      return prev;
-    },
   },
 });
