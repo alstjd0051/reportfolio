ef1c5ade015ec63a3ed5884ea1509636b854710c
alstjd0051 <wsc03002@naver.com>
2023-06-27 13:00:34 +0900

    UPDATE:: Add Footer Components

---
+++ components/commons/layout/Footer.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef, useState } from "react";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  return (
+    <div className="absolute flex justify-between p-5 bg-gray-100/30 -bottom-24  w-full flex-1 text-white ">
+      <div className="flex items-center gap-3">
+        <h1>Copyright ⓒ 2023</h1>
+        <p>This is CMSProject</p>
+      </div>
+      <div className=" ">
+        <span>{time.toLocaleTimeString()}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
