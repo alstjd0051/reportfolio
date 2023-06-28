79eb5eacfe461795c09295378750a50891f65df7
alstjd0051 <wsc03002@naver.com>
2023-06-29 02:24:19 +0900

    UPDATE:: NextjsIDAPI

---
+++ components/utils/fetchNextjsId.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { NextJS } from "../lib/typings";
+
+export default async function fetchNextjsId(title: any) {
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/next/${title}`
+  );
+  return data.nextjs;
+}
---
+++ pages/api/next/[title].ts
@@ -0,0 +1,22 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import { groq } from "next-sanity";
+import { NextJS } from "../../../components/lib/typings";
+import { sanityClient } from "../../../sanity";
+
+type Data = {
+  nextjs: NextJS[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const title = req.query.title;
+  const query = groq`
+    *[_type == 'nextjs' && title== '${title}'] {
+      ...,
+    }
+`;
+  const nextjs: NextJS[] = await sanityClient.fetch(query);
+  res.status(200).json({ nextjs });
+}
