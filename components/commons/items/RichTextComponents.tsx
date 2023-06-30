8431915f0d1ba0eacb98b90a28816eb66f7f5644
alstjd0051 <wsc03002@naver.com>
2023-06-30 22:05:35 +0900

    addCodeBlock

---
+++ components/commons/items/RichTextComponents.tsx
@@ -0,0 +1,82 @@
+import Image from "next/image";
+import Link from "next/link";
+import { atelierSeasideDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { urlFor } from "../../../sanity";
+
+export const RichTextComponents = {
+  types: {
+    image: ({ value }: any) => {
+      return (
+        <>
+          <Image
+            src={urlFor(value).url()}
+            alt="Blog Post Image"
+            className="object-cover"
+          />
+        </>
+      );
+    },
+    code: ({ value }: any) => {
+      console.log(value.language);
+      return (
+        <>
+          <div className="">
+            <SyntaxHighlighter
+              PreTag={"div"}
+              style={atelierSeasideDark}
+              language={value.language}
+            >
+              {value.code}
+            </SyntaxHighlighter>
+          </div>
+        </>
+      );
+    },
+  },
+
+  list: {
+    bullet: ({ children }: any) => (
+      <ul className="ml-10  py-5 list-disc space-y-5">{children}</ul>
+    ),
+    number: ({ children }: any) => (
+      <ol className="mt-10 list-decimal">{children}</ol>
+    ),
+  },
+  block: {
+    h1: ({ children }: any) => (
+      <h1 className="text-5xl py-10 font-bold">{children}</h1>
+    ),
+    h2: ({ children }: any) => (
+      <h2 className="text-4xl py-10 font-bold">{children}</h2>
+    ),
+    h3: ({ children }: any) => (
+      <h3 className="text-3xl py-10 font-bold">{children}</h3>
+    ),
+    h4: ({ children }: any) => (
+      <h4 className="text-2xl py-10 font-bold">{children}</h4>
+    ),
+
+    blockquote: ({ children }: any) => (
+      <blockquote className="border-l-[#f7ab0a] border-l-4 pl-5 py-5 my-5">
+        {children}
+      </blockquote>
+    ),
+  },
+  marks: {
+    link: ({ children, value }: any) => {
+      const rel = !value.href.startsWith("/")
+        ? "noreferrer nopener"
+        : undefined;
+      return (
+        <Link
+          href={value.href}
+          rel={rel}
+          className="underline decoration-[#f7ab0a] hover:decoration-black"
+        >
+          {children}
+        </Link>
+      );
+    },
+  },
+};
