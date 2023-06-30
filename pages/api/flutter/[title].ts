b08f5d466b85d04aa6a6d4dae44bf1fefc84312b
alstjd0051 <wsc03002@naver.com>
2023-06-30 17:29:48 +0900

    UPDATE:: FlutterIDAPI

---
+++ pages/api/flutter/[title].ts
@@ -0,0 +1,22 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import { groq } from "next-sanity";
+import { Flutter } from "../../../components/lib/typings";
+import { sanityClient } from "../../../sanity";
+
+type Data = {
+  flutter: Flutter[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const title = req.query.title;
+  const query = groq`
+    *[_type == 'flutter' && title== '${title}'] {
+      ...,
+    }
+`;
+  const flutter: Flutter[] = await sanityClient.fetch(query);
+  res.status(200).json({ flutter });
+}
