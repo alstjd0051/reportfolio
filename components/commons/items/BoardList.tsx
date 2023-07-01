fbd1125cb6d2d5b0477d5ba64142a41f532c3388
alstjd0051 <wsc03002@naver.com>
2023-07-01 23:17:16 +0900

    ADD Component List

---
+++ components/commons/items/BoardList.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+type Props = {
+  Num?: number;
+  title: string;
+  createdAt: Date;
+  route: string;
+};
+
+const BoardList = ({ title, Num, createdAt, route }: Props) => {
+  const router = useRouter();
+  return (
+    <div className="flex items-center w-full mt-10 py-3 border-b-2 border-white/50  ">
+      <div className="px-4">
+        <h1>{Num}</h1>
+      </div>
+      <div
+        onClick={() => router.push(`${route}/${title}`)}
+        className="flex-1 cursor-pointer "
+      >
+        <p>{title}</p>
+      </div>
+      <div className="basis-1/5 ">
+        <p>{new Date(createdAt).toLocaleDateString()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BoardList;
