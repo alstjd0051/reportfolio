23ce95ff1ea4f4abf6f1eea5a7ba47597b830528
alstjd0051 <wsc03002@naver.com>
2023-07-02 20:45:23 +0900

    published

--- pages/nestjs/[title]/index.tsx
+++ pages/nestjs/[title]/index.tsx
@@ -10,6 +10,7 @@ import "easymde/dist/easymde.min.css";
 import fetchNestjsId from "../../../components/utils/nestjs/fetchNestjsID";
 import fetchNestjs from "../../../components/utils/nestjs/fetchNestjs";
 import { useRouter } from "next/router";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {
   pageInfo?: PageInfo;
@@ -38,9 +39,9 @@ export default function NextIdPage({ pageInfo, socials, nestjs }: Props) {
       <Header Home contact pageInfo={pageInfo} socials={socials} />
 
       <main className="py-5 px-20 max-w-7xl">
-        <div className=" w-full flex items-center  ">
+        <div className=" w-full flex items-center ">
           {nestjs.map((item) => (
-            <div className="w-full" key={item._id}>
+            <div className="w-full h-screen relative" key={item._id}>
               <h1 className="flex-1  py-2 font-bold  pl-3 dark:bg-white/40 bg-black text-white/10 dark:text-white/60 items-center flex gap-3 ">
                 <p
                   className="cursor-pointer"
@@ -56,6 +57,23 @@ export default function NextIdPage({ pageInfo, socials, nestjs }: Props) {
                   components={RichTextComponents}
                 />
               </div>
+              {item?.link ? (
+                <div className=" flex items-center gap-1 absolute bottom-0 right-0">
+                  <h1>
+                    <span className="border-b border-red-400 pb-2">
+                      Authorship :
+                    </span>{" "}
+                    Click to Icon
+                  </h1>
+                  <SocialIcon
+                    fgColor="gray"
+                    bgColor="transparent"
+                    url={item?.link}
+                    target={"_blank"}
+                    className="hover:fill-gray-300"
+                  />
+                </div>
+              ) : undefined}
             </div>
           ))}
         </div>
