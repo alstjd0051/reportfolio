febcd924da704f0093fae8e7a96eac11c47e65f5
alstjd0051 <wsc03002@naver.com>
2023-07-05 16:51:33 +0900

    update:: published text

--- components/commons/items/BoardList.tsx
+++ components/commons/items/BoardList.tsx
@@ -12,17 +12,14 @@ type Props = {
 const BoardList = ({ title, Num, createdAt, route, id }: Props) => {
   const router = useRouter();
   return (
-    <div className="flex items-center  w-full mt-10 py-3 border-b-2 border-white/50  ">
-      <div className="px-4">
-        <h1>{Num}</h1>
-      </div>
+    <div className="flex items-center  w-full mt-10 py-3 border-b-2 border-white/50 justify-between ">
       <div
         onClick={() => router.push(`${route}`)}
-        className="flex-1 cursor-pointer "
+        className="flex cursor-pointer    "
       >
-        <p>{title}</p>
+        <p className="text-clip  ">{title}</p>
       </div>
-      <div className="basis-1/5 ">
+      <div className="basis-1/12 text-end ">
         <p>{new Date(createdAt).toLocaleDateString()}</p>
       </div>
     </div>
