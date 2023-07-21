0ddb6b3d09105b74d12a9c779d308fe501097dfb
alstjd0051 <wsc03002@naver.com>
2023-07-21 13:08:54 +0900

    update:: republish

--- components/commons/blog/BlogContent.tsx
+++ components/commons/blog/BlogContent.tsx
@@ -3,10 +3,6 @@ import useSWR from "swr";
 import BgModel from "../../unit/threejs/model";
 import { Content } from "../../lib/typings";
 import ContentCard from "../items/ContentCard";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
 
 const BlogContent = () => {
   const { data } = useSWR<Content[]>("/api/content");
@@ -14,19 +10,14 @@ const BlogContent = () => {
   return (
     <div className="h-screen relative ">
       <div className="flex gap-5 items-center h-full justify-center  ">
-        <div className="max-w-3xl">
-          <Swiper
-            slidesPerView={2}
-            navigation={true}
-            modules={[Navigation]}
-            className="space-x-4"
-          >
+        <div className="max-w-3xl md:block hidden">
+          <div id="container">
             {data?.map(({ id, code, title }, i) => (
-              <SwiperSlide className="" key={i}>
+              <div id="item" key={i}>
                 <ContentCard code={code} title={title} id={id} />
-              </SwiperSlide>
+              </div>
             ))}
-          </Swiper>
+          </div>
         </div>
       </div>
 
--- components/commons/items/ContentCard.tsx
+++ components/commons/items/ContentCard.tsx
@@ -11,11 +11,14 @@ type Props = {
 
 const ContentCard = ({ code, id, title }: Props) => {
   return (
-    <div className="text-black overflow-y-scroll rounded-lg min-h-[20rem] bg-white w-[20rem] shadow-lg flex flex-col px-5 " onClick={() =>console.log(id)} >
+    <div
+      className="text-black overflow-y-scroll rounded-lg min-h-[20rem] bg-white w-[20rem] shadow-lg flex flex-col px-5 "
+      onClick={() => console.log(id)}
+    >
       <div className="flex gap-3">
         <h1 className="font-bold p-1 text-xl ">{title}</h1>
       </div>
-      <div className="whitespace-pre-line overflow-y-scroll">
+      <div className="whitespace-pre-line overflow-y-scroll pt-5">
         <PortableText value={code} components={RichTextComponents} />
       </div>
     </div>
