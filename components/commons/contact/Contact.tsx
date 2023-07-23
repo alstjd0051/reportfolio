c584ae93cdd7ad7aff27e0bf6fa5cc070258bf1b
alstjd0051 <wsc03002@naver.com>
2023-07-24 00:50:19 +0900

    remove email modal

--- components/commons/contact/Contact.tsx
+++ components/commons/contact/Contact.tsx
@@ -11,36 +11,25 @@ import FormModal from "../modal/FormModal";
 import { PageInfo } from "../../lib/typings";
 import { PortableText } from "@portabletext/react";
 import { RichTextComponents } from "../items/RichTextComponents";
-import MyResume from "../items/MyResume";
 import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
 
+const MyResume = dynamic(() => import("../items/MyResume"), {
+  loading: () => <p>Loading...</p>,
+});
 type Props = {
   pageInfo: PageInfo;
 };
 
 const ContactMe = ({ pageInfo }: Props) => {
-  const [OpenForm, setOpenForm] = useState<boolean>(false);
   const [modalOpen, setModalOpen] = useState(false);
 
   const onClickResumeModal = () => {
     setModalOpen(!modalOpen);
   };
 
-  const onClickFormModal = () => {
-    setOpenForm(!OpenForm);
-  };
-
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly items-center mx-auto  ">
-      {OpenForm && (
-        <>
-          <div
-            className="w-full h-full absolute"
-            onClick={onClickFormModal}
-          ></div>
-          <FormModal onClickFormModal={onClickFormModal} />
-        </>
-      )}
       <h3 className="hidden lg:absolute top-24 uppercase tracking-[20px] text-gray-500 md:text-2xl ">
         Contact
       </h3>
@@ -64,12 +53,9 @@ const ContactMe = ({ pageInfo }: Props) => {
                 .replace(/\-{1,2}$/g, "")}
             </p>
           </div>
-          <div
-            onClick={onClickFormModal}
-            className="flex items-center space-x-5 justify-center"
-          >
+          <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-[#F5DF4D] h-7 w-7 animate-pulse " />
-            <p className="text-2xl cursor-pointer">{pageInfo.email}</p>
+            <p className="text-2xl">{pageInfo.email}</p>
           </div>
           <motion.div
             whileHover={{ scale: 1.1 }}
