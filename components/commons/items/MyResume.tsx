5148ee6379b7adf8a4ea8a487d5fa460d32a1bdb
alstjd0051 <wsc03002@naver.com>
2023-07-31 01:21:26 +0900

    suspentse

--- components/commons/items/MyResume.tsx
+++ components/commons/items/MyResume.tsx
@@ -3,13 +3,18 @@ import useSWR from "swr";
 import { Contact } from "../../lib/typings";
 import PDFViewer from "./pdfViewer";
 import { motion } from "framer-motion";
+import { MoonLoader } from "react-spinners";
 
 type Props = {
   onClick: () => void;
 };
 
 const MyResume = ({ onClick }: Props) => {
-  const { data } = useSWR<Contact>("/api/contact");
+  const { data, isLoading } = useSWR<Contact>("/api/contact");
+
+  if (isLoading) {
+    <MoonLoader color="red" />;
+  }
 
   return (
     <div className="relative overflow-hidden">
